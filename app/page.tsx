11782abca056e089edbf0bdbce66286286329851
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-08-24 09:57:28 +0900

    feat: パーツごとに取り出して表示

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,41 @@
 
 import { Canvas } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 
 function Shoe() {
-  const { scene } = useGLTF("/models/shoe-draco.glb");
-  return <primitive object={scene} />;
+  type GLTFResult = {
+    nodes: {
+      [key: string]: THREE.Mesh;
+    };
+    materials: {
+      [key: string]: THREE.MeshStandardMaterial;
+    };
+  };
+  const { nodes, materials } = useGLTF(
+    "models/shoe-draco.glb",
+  ) as unknown as GLTFResult;
+  console.log("Nodes (パーツ一覧):", nodes);
+  console.log("Materials (マテリアル一覧):", materials);
+  return (
+    <group>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+    </group>
+  );
 }
 
 export default function Home() {
   return (
     <div className="canvasContainer">
       <Canvas>
-        {/* 光がないと真っ黒になるためライトを追加 */}
         <ambientLight intensity={1} />
-        {/* 球体の代わりに靴を表示 */}
         <Shoe />
       </Canvas>
     </div>
